07b567f179e382085ebc258c5926a7f30c798c7e
kimsaehwang <vitamin5003@naver.com>
2024-01-22 16:45:15 +0900

    start

---
+++ Assets/Scripts/PlayerCtrllerAri.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrllerAri : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
